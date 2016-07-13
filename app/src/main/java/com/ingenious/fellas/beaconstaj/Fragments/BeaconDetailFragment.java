d72b45fc7337eb0c0fc562563e9e804be8d2ad46
Mustafa <mustafaerdogan1994@gmail.com>
2016-07-13 14:24:00 +0300

    lostButton is functionalized

--- app/src/main/java/com/ingenious/fellas/beaconstaj/Fragments/BeaconDetailFragment.java
+++ app/src/main/java/com/ingenious/fellas/beaconstaj/Fragments/BeaconDetailFragment.java
@@ -1,22 +1,13 @@
 package com.ingenious.fellas.beaconstaj.Fragments;
 
 import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.le.BluetoothLeScanner;
-import android.bluetooth.le.ScanCallback;
-import android.bluetooth.le.ScanFilter;
-import android.bluetooth.le.ScanResult;
-import android.bluetooth.le.ScanSettings;
 import android.content.Context;
 import android.content.Intent;
-import android.media.AudioManager;
-import android.media.ToneGenerator;
 import android.os.AsyncTask;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,20 +15,16 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.ingenious.fellas.beaconstaj.Activities.BeaconDetailActivity;
 import com.ingenious.fellas.beaconstaj.Activities.BeaconFinderActivity;
-import com.ingenious.fellas.beaconstaj.Activities.BeaconListActivity;
 import com.ingenious.fellas.beaconstaj.Activities.BeaconMapActivity;
 import com.ingenious.fellas.beaconstaj.Classes.Beacon;
 import com.ingenious.fellas.beaconstaj.Classes.Globals;
 import com.ingenious.fellas.beaconstaj.Classes.RequestHandler;
 import com.ingenious.fellas.beaconstaj.R;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class BeaconDetailFragment extends Fragment {
@@ -49,6 +36,7 @@ public class BeaconDetailFragment extends Fragment {
     private Button lostButton;
     private Button mapOrFindButton;
     private Beacon mBeacon;
+    private int isWanted = 0;
 
     public BeaconDetailFragment() {
     }
@@ -64,6 +52,7 @@ public class BeaconDetailFragment extends Fragment {
             if (appBarLayout != null) {
                 appBarLayout.setTitle(mBeacon.getName());
             }
+            new isWantedTask().execute(mBeacon.getAddress());
         }
         FloatingActionButton fab = (FloatingActionButton) getActivity().findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -109,6 +98,78 @@ public class BeaconDetailFragment extends Fragment {
                 context.startActivity(intent);
             }
         });
+        lostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 1 - isWanted;
+                Log.i("hello",String.valueOf(a));
+                new setWantedTask().execute(mBeacon.getAddress(),String.valueOf(a));
+            }
+        });
+
         return rootView;
+    }
+
+    public class isWantedTask extends AsyncTask<String, Void, JSONObject> {
+
+        @Override
+        protected JSONObject doInBackground(String... params) {
+            HashMap<String, String> h = new HashMap<>();
+            h.put("mac", String.valueOf(params[0]));
+            JSONObject response = RequestHandler.sendPostRequest(Globals.URL + "isWanted.php", h);
+            try {
+                Log.i(Globals.TAG, response.getString("status_message"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Log.i(Globals.TAG, "response'u alamadik");
+            }
+
+            JSONObject obj = new JSONObject();
+            try {
+                if (response.getJSONObject("data") != null)
+                    obj = response.getJSONObject("data");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return obj;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject jsonObject) {
+            if (jsonObject == null) return;
+            try {
+                String str = jsonObject.getString("isWanted");
+                isWanted = Integer.valueOf(str);
+                Log.i("hello", "isWanted: " + isWanted);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public class setWantedTask extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected Void doInBackground(String... params) {
+            HashMap<String, String> h = new HashMap<>();
+            h.put("mac", String.valueOf(params[0]));
+            h.put("isWanted",String.valueOf(params[1]));
+            Log.i("hello","newparam: "+String.valueOf(params[1]));
+            JSONObject response = RequestHandler.sendPostRequest(Globals.URL + "setWanted.php", h);
+            try {
+                Log.i(Globals.TAG, response.getString("status_message"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Log.i(Globals.TAG, "response'u alamadik");
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            new isWantedTask().execute(mBeacon.getAddress());
+        }
     }
 }
